e6f83b97ca4a450574c9bec93d39d62bc42add90
Horther <Felipe.Venancio-Silva@outlook.com>
2023-11-29 11:32:39 +0000

    Navbar e Footer
    Componentes e CSS para os ditos cujos

--- costs/src/App.js
+++ costs/src/App.js
@@ -1,24 +1,27 @@
-import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './components/pages/Home';
+import Projects from './components/pages/Projects';
 import Company from './components/pages/Company';
 import Contact from './components/pages/Contact';
 import NewProject from './components/pages/NewProject';
 
 import Container from './components/layout/Container';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
 
 function App() {
   return (
     <Router>
-        <div>
-          <Link to='/'>Home</Link>
-          <Link to='/company'>Empresa</Link>
-          <Link to='/contact'>Contato</Link>
-          <Link to='/newProject'>Novo Projeto</Link>
-        </div>
+        
+        <Navbar/>
+
         <Container customClass='min_height'>
+          
           <Routes>
             
               <Route exact path='/' element={<Home/>}> </Route>
+
+              <Route exact path='/projects' element={<Projects/>}> </Route>
 
               <Route exact path='/company' element={<Company/>}> </Route>
 
@@ -27,8 +30,11 @@ function App() {
               <Route exact path='/newProject' element={<NewProject/>}> </Route>
             
           </Routes>
+        
         </Container>
-        <p>Footer</p>
+
+        <Footer/>
+
     </Router>
     
   );
