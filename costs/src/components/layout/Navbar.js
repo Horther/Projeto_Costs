95acc9a53608b71030a91dee6523a4fba983b240
Horther <Felipe.Venancio-Silva@outlook.com>
2023-11-29 13:00:46 +0000

    Home e LinkButton
    Construção e estilização da Home/ Criação e estilização do LinkButton

--- costs/src/components/layout/Navbar.js
+++ costs/src/components/layout/Navbar.js
@@ -13,13 +13,13 @@ function Navbar() {
                     <img src={logo} alt="Costs" />
                 </Link>
                 <ul className={styles.list}>
-                    <li className={styles.item}><Link to='/ '>Home</Link>
+                    <li className={styles.item}><Link to='/'>Home</Link>
                     </li>
                     <li className={styles.item}><Link to='/projects'>Projetos</Link>
                     </li>
-                    <li className={styles.item}><Link to='/ company'>Empresa</Link>
+                    <li className={styles.item}><Link to='/company'>Empresa</Link>
                     </li>
-                    <li className={styles.item}><Link to='/ contact'>Contato</Link>
+                    <li className={styles.item}><Link to='/contact'>Contato</Link>
                     </li>
                </ul>
             </Container>
