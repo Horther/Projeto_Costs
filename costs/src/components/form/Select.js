ff16a768dd85e86d81e48068520b0002f6eb532a
Horther <Felipe.Venancio-Silva@outlook.com>
2023-12-26 09:09:37 +0000

    gambiarra momentânea
    n sei oque dizer

--- costs/src/components/form/Select.js
+++ costs/src/components/form/Select.js
@@ -1,6 +1,8 @@
 import styles from './Select.module.css'
 
-function Select({text, name, options, handleOnChange, value}) {
+import {useEffect, useState} from 'react'
+
+/*function Select({text, name, options, handleOnChange, value}) {
     return (
         <div className={styles.form_control}>
             <label htmlFor={name}>{text}:</label>
@@ -12,6 +14,40 @@ function Select({text, name, options, handleOnChange, value}) {
                 <option>Selecione uma opção</option>
                 {options && options.length > 0 && options.map((option) => (
                     <option value={option.id} key={option.id}> {option.name}
+                    </option>
+                ))}
+            </select>
+        </div>
+    )
+}
+
+export default Select
+
+*/
+
+function Select({text, name, options, value, handleOnChange}){
+
+    const [categories, setCategories] = useState([])
+
+    function chooseOption(e){
+        fetch("http://localhost:5000/categories", {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+        .then((resp) => resp.json())
+        .then((data) => setCategories(data))
+        .catch((err) => console.log(err))
+    }
+
+    return (
+        <div className={styles.form_control}>
+            <label htmlFor={name}>{text}</label>
+            <select name={name} id={name} onClick={chooseOption}>
+                {categories.map((option) => (
+                    <option value={option.id} key={option.id}>
+                        {option.name}
                     </option>
                 ))}
             </select>
